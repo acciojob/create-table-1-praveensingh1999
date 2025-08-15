2840b7a56d7e69cfa5182d486b0fb70a04589ca3
Praveen Singh <57175335+praveensingh1999@users.noreply.github.com>
2025-08-15 20:52:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,20 +4,21 @@ function insert_Row() {
 	
 	let table = document.getElementById("sampleTable");
 
+    // Create a new row
     let newRow = document.createElement("tr");
 
+    // Create first cell
     let cell1 = document.createElement("td");
     cell1.innerText = "New Cell1";
     newRow.appendChild(cell1);
 
+    // Create second cell
     let cell2 = document.createElement("td");
     cell2.innerText = "New Cell2";
     newRow.appendChild(cell2);
 
-    if (table.rows.length > 0) {
-        table.insertBefore(newRow, table.rows[0]);
-    } else {
-        table.appendChild(newRow);
-    }
-     
+    // Append row to table
+    // table.appendChild(newRow);
+	table.insertBefore(newRow, table.children[0]);
+  
 }
